b1a2c8fe9e53ed6c6d75210583c88cd205c409e8
MarieanCC <cosmin.mariean@wise.com>
2026-04-13 18:53:51 +0300

    html file

--- Nodejs-app/server.js
+++ Nodejs-app/server.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express()
 
-function bla() {
-    console.log(" app listening on port 3000")
-}
-
-
-app.listen(3000, ) 
+app.listen(3000, function () {
+  console.log('app listening on port 3000!')
+}) 
